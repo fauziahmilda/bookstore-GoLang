7fce894df9b93f252e38dd152b8cb28532d7516a
fauziahmilda <fauziah.milda@gmail.com>
2022-11-29 22:15:21 +0700

    third commit

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,18 @@
-//main file ini keempat, memberitahu golang ini ada routes
+// main file ini keempat, memberitahu golang ini ada routes
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/fauziahmilda/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
