0b49b8401c8b62fdbe80e2103df3a0132dace8b8
fauziahmilda <fauziah.milda@gmail.com>
2022-11-29 22:38:42 +0700

    done

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,10 +1,10 @@
-//ini kedua ditulis
-
+// ini kedua ditulis
+// this file for return var call db, help others file interact w db
 package config
 
 import (
 	"github.com/jinzhu/gorm"
-	// "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
@@ -12,7 +12,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root") //ini belum beres, agatau username sama kata sandi dari mysql
+	d, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/simplerest?charset=utf8&parseTime=True&loc=Local") //ini belum beres, agatau username sama kata sandi dari mysql
 	//charset=utf8&parseTime=True&loc=Local
 	if err != nil {
 		panic(err)
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -1,1 +1,57 @@
-//file ini lbh besar, jadi terakhir setelah util
+// file ini lbh besar, jadi terakhir setelah util
+package models
+
+import (
+	"github.com/fauziahmilda/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm: "" json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
+
+// CREATE FUNCTION
+func (b *Book) CreateBook() *Book {
+	//db help to talk to database
+	db.NewRecord(b)
+	//b isi something will we receive of type book
+	db.Create(&b)
+	return b
+}
+
+// GET ALL FUNCTION
+func GetAllBooks() []Book { //using slice return list of database
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+// GET A BOOK BY ID FUNCTION
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+	return &getBook, db
+}
+
+// DELETE FUNCTION
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
+
+// UPDATE FUNCTION
+// func UpdateBook() {
+// 	var
+// }
